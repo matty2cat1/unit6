b3fa5f37f60cccf40be093dfdf0609a1cc884a33
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-09 08:39:07 -0400

    Updated from Brython Server: 5/9/2018 8:39:07 AM

--- fileDemo.py
+++ fileDemo.py
@@ -1,1 +1,13 @@
+#Matt Westelman
+#5/9/18
+#fileDemo.py - How to read a file
 
+file = open('engmix.txt')
+
+numWords = 0
+for line in file:
+    if 'west' in line:
+        print(line.strip())
+    numWords += 1
+    
+print(numWords)

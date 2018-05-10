bcd52db310f2c4619f4c35bd8cccc2a1b58eadae
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-10 08:46:27 -0400

    Updated from Brython Server: 5/10/2018 8:46:26 AM

--- howManyWords.py
+++ howManyWords.py
@@ -1,1 +1,16 @@
+#Matt Westelman
+#5/10/18
+#howManyWords.py - all these words, and yet my vocabulary consists of under 200 words
 
+list = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+file = open('engmix.txt')
+
+def update(A)
+    for line in file:
+        line.strip()
+        return([len(line)] = [len(line)+1])
+        
+print(update(list))
+        
+    
+    

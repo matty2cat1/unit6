7d898cd29676851b18550d4cdbd717b7ff021dea
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-10 08:47:28 -0400

    Updated from Brython Server: 5/10/2018 8:47:27 AM

--- howManyWords.py
+++ howManyWords.py
@@ -5,10 +5,10 @@
 list = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 file = open('engmix.txt')
 
-def update(A)
-    for line in file:
-        line.strip()
-        return([len(line)] = [len(line)+1])
+def update(A):
+        for line in file:
+            line.strip()
+            return([len(line)] = [len(line)+1])
         
 print(update(list))
         

a7ea821fa79e4d932ef77fd63d5ae64463e467c1
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-10 08:09:29 -0400

    Updated from Brython Server: 5/10/2018 8:09:28 AM

--- lastWordDemo.py
+++ lastWordDemo.py
@@ -1,1 +1,12 @@
+#Matt Westelman 
+#5/10/18
+#lastWordDemo.py
 
+file = open('fileDemo.py')
+
+for line in file:
+    words = line.split()
+    if len(words)>0:
+        print(words[-1])
+    else:
+        print()

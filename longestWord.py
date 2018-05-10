be1d0685e8a16bfb43c46070f98a19d29b705434
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-10 08:25:45 -0400

    Updated from Brython Server: 5/10/2018 8:25:45 AM

--- longestWord.py
+++ longestWord.py
@@ -1,1 +1,11 @@
+#Matt Westelman
+#5/10/18
+#longestWord.py
 
+file = open('engmix.txt')
+
+longestWord=''
+for line in file:
+    if len(line) > len(longestWord):
+        line=longestWord
+    print(line)

2f5986909e35f3657352d1c60773678aee518cf1
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-11 08:29:02 -0400

    Updated from Brython Server: 5/11/2018 8:29:01 AM

--- warmUp16.py
+++ warmUp16.py
@@ -1,1 +1,11 @@
+#Matt Westelman
+#5/9/18
+#warmUp16.py - M&W
+
+file = open('engmix.txt')
+
+for line in file:
+    line = line.strip
+    if 'm' == line[0] and 'w'== line[-1]:
+        print(line)
 

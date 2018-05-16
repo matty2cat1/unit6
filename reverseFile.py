e0791b75391ed2b9a9ca2506f536dd1d9181dae8
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-16 08:39:20 -0400

    Update reverseFile.py

--- reverseFile.py
+++ reverseFile.py
@@ -1,1 +1,10 @@
+#Matt Westelman
+#5/16/18
+#palindromes.py - Well, I guess last program's mistake comes in handy here
 
+file = open('engmix.txt')
+
+for line in file:
+    fred = line.strip
+    if str(fred) = str(fred[::-1])
+        print(fred)

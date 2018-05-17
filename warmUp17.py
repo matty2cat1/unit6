f7d0ee16ca9e3617edae38a3e12ac45474780b27
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-17 08:18:45 -0400

    Updated from Brython Server: 5/17/2018 8:18:44 AM

--- warmUp17.py
+++ warmUp17.py
@@ -1,1 +1,11 @@
+#Matt Westelman
+#5/17/18
+#warmUp17.py - final warm up...
 
+
+'w','e','s','t','l','m','a','n'
+file = open('engmix.txt')
+
+for line in file:
+    if 'w' in line and 'e' in line and 's' in line and 't' in line and 'l' in line and 'm' in line and 'a' in line and 'n' in line:
+        print(line)

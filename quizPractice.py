927ba0ed6ea7686490b6b84788895be2e8275a00
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-18 08:40:46 -0400

    Updated from Brython Server: 5/18/2018 8:40:45 AM

--- quizPractice.py
+++ quizPractice.py
@@ -1,3 +1,15 @@
 #MW
 #QuizPractice.py
 #date
+
+list = []
+
+file = open('engmix.txt')
+
+def update(A):
+        for line in file:
+            line.strip()
+            A.append(line)
+
+update(list)
+print(list[len(list)/2])

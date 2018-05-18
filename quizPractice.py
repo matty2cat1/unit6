e7bed8d19ec4145d0ce293232f97132963369958
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-18 08:45:19 -0400

    Updated from Brython Server: 5/18/2018 8:45:18 AM

--- quizPractice.py
+++ quizPractice.py
@@ -3,6 +3,7 @@
 #date
 
 list = []
+L = []
 
 file = open('engmix.txt')
 
@@ -10,6 +11,10 @@ def update(A):
         for line in file:
             line.strip()
             A.append(line)
+
+for line in file:
+    if len(line)>0 and 'r' == line[0]:
+            print(line)
 
 update(list)
 print(list[len(list)/2])

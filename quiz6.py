34adb59b67d1dc00a99549bee651ec06f255565c
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-21 08:18:24 -0400

    Updated from Brython Server: 5/21/2018 8:18:23 AM

--- quiz6.py
+++ quiz6.py
@@ -1,3 +1,14 @@
 #Matt Westelman
 #5/21/18
 #quiz6.py - Final quiz?
+
+L = []
+
+file = open('engmix.txt')
+
+letter = input("Enter a letter ")
+
+for line in file:
+    lettcou = line.count(letter)
+    if lettcou == 4:
+        print(line)

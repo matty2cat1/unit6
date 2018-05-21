a757f1260d16c33d721d69430385402f310870f3
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-21 08:36:57 -0400

    Updated from Brython Server: 5/21/2018 8:36:56 AM

--- quiz6.py
+++ quiz6.py
@@ -2,13 +2,39 @@
 #5/21/18
 #quiz6.py - Final quiz?
 
-L = []
+number = 0
 
 file = open('engmix.txt')
 
-letter = input("Enter a letter ")
+let = input("Enter a letter ")
 
+num = int(input("Enter a number"))
+
+
+""" PROGRAM 1
 for line in file:
-    lettcou = line.count(letter)
+    lettcou = line.count(let)
     if lettcou == 4:
         print(line)
+"""
+
+for line in file:
+    line = line.strip
+
+""" PROGRAM 3 
+for line in file:
+    line = line.strip()
+    if len(line) == num and line[0] == let:
+        print(line)
+"""
+
+""" PROGRAM 4
+for line in file:
+    line = line.strip()
+    if len(line) >= 10:
+        number+=1
+        if number == 8000:
+            print(line)
+            break
+"""
+
